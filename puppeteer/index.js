c31f04dc8056fce636f348f1c5524a17596b1524
jjhelguero <joshua.helguero@mercari.com>
2023-01-17 23:34:16 -0600

    add another check

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -5,8 +5,9 @@ const assert = require('assert');
     const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
     const IMAGE_LENGTH = 4
-    const IMAGE_UNIQUE_TEXT = 1
+    const IMAGE_UNIQUE_LENGTH = 1
     const IMAGE_DESCRIPTION = 'A random image courtesy of unsplash.com.'
+    const IMAGE_SRC = 'https://source.unsplash.com/random/400x200'
     await page.goto('http://localhost:3000/')
 
     const pageTitle = await page.$eval('h1', el => el.innerText)
@@ -21,9 +22,15 @@ const assert = require('assert');
     assert.equal(imagesDescription.length, IMAGE_LENGTH)
 
     const uniqueArray =[... new Set(imagesDescription)]
-    assert.equal(uniqueArray.length, IMAGE_UNIQUE_TEXT)
+    assert.equal(uniqueArray.length, IMAGE_UNIQUE_LENGTH)
 
     assert.equal(uniqueArray[0], IMAGE_DESCRIPTION)
+
+    const imageSrc = await page.$$eval('.section-style img', images => images.map(image => image.src))
+    const uniqueImageSrc =[... new Set(imageSrc)]
+    assert.equal(uniqueImageSrc.length, IMAGE_UNIQUE_LENGTH)
+
+    assert.equal(uniqueImageSrc[0], IMAGE_SRC)
 
     await page.screenshot({path: 'home.png', fullPage: true})
 
